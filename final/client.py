2e7d2d62c0b62436853554bf1b622bc40fe3b863
viditjain2119 <viditjain2119@gmail.com>
2018-04-26 19:21:05 +0530

    Client done

--- final/client.py
+++ final/client.py
@@ -100,31 +100,6 @@ class client(object):
             except Exception as e:
                 print 'Connection refused'
 
-    def config_change(self, port, new_config, uuid):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        msg = ConfigChange(new_config, uuid, 1)
-        s.sendto(pickle.dumps(msg),("",port))
-        # we need to get committed twice
-        while 1:
-            try:
-                reply, addr = s.recvfrom(1024)
-                if reply != '':
-                    self.num_of_reply != 1
-                    print reply
-                    break
-            except Exception as e:
-                print 'Connection refused'
-        msg = ConfigChange(new_config, uuid, 2)
-        s.sendto(pickle.dumps(msg), ("",port))
-        while 1:
-            try:
-                reply, addr = s.recvfrom(1024)
-                if reply != '':
-                    print reply
-                    break
-            except Exception as e:
-                print 'Connection refused'
-        s.close()
 
 def main():
     try:
@@ -137,22 +112,14 @@ def main():
 
     while True:
         customer = client()
-        server_id = input('Which datacenter do you want to connect to? 1-%d: ' % num_ports )
+        server_id = input('Whom to connect ? 1-%d: ' % num_ports )
         request = raw_input('How can we help you? --')
-        if request == 'show':
-            requestThread = KThread(target = customer.show_state, args = (ports[server_id - 1],))
-            timeout = 5
-        elif request.split()[0] == 'change':
-            uuid_ = uuid.uuid1()
-            msg_split = request.split()
-            new_config_msg = msg_split[1:]
-            new_config = [int(item) for item in new_config_msg]
-            print new_config
-            requestThread = KThread(target = customer.config_change, args = (ports[server_id - 1], new_config, uuid_))
-            timeout = 20
-        else:
+        if request == 'buy':
             uuid_ = uuid.uuid1()
             requestThread = KThread(target = customer.buyTickets, args =  (ports[server_id - 1], request, uuid_))
+            timeout = 5
+        else:
+            requestThread = KThread(target = customer.show_state, args = (ports[server_id - 1],))
             timeout = 5
         start_time = time.time()
         requestThread.start()
@@ -160,8 +127,9 @@ def main():
             if not requestThread.is_alive():
                 break
         if requestThread.is_alive():
-            print 'Timeout! Try again'
             requestThread.kill()
+            msgP = 'Timeout! Try again'
+            print msgP
 
 if __name__ == '__main__':
     main()
